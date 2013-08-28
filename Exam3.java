786922bacd5463cd9a8f9eaf35b4cc77be6c938d
Woonohyo <woonohyo@woonohyo-VirtualBox.(none)>
2013-08-28 23:07:45 +0900

    Final Term

--- Exam3.java
+++ Exam3.java
@@ -5,36 +5,22 @@ public class Exam3 implements Runnable {
 	static final double PI = 3.14159265359;
 	final static int NT = 10;
 	final static int MAX_DOT = 10000000;
-	static int TotalDot = 0;
+	final static int QUANTUM = MAX_DOT / NT;
 	static int DotLessThanOne = 0;
-	static int id = 0;
 	static double result;
-	Point dot = new Point();
-	Random rand = new Random();
 
 	@Override
 	public void run() {
 		int i;
-		int localTotal = 0;
 		int localLessOne = 0;
-		int start;
-		int end;
-		synchronized (this) {
-				start = (MAX_DOT / NT) * id + 1;
-				end = (MAX_DOT / NT) * (id + 1);
-				id++;
-		}
+		Random rand = new Random();
+		Point dot = new Point();
 
-		for ( i = start; i <= end; i++ ) { 
-			if (dot.distance(rand.nextDouble(), rand.nextDouble()) <= 1) {
-				localTotal++;
+		for ( i = 0; i < QUANTUM / NT; i++ ) 
+			if (dot.distance(rand.nextDouble(), rand.nextDouble()) <= 1)
 				localLessOne++;
-			} else
-				localTotal++;
-		}
 
 		synchronized(this) {
-			TotalDot += localTotal;
 			DotLessThanOne += localLessOne;
 		}
 	}
@@ -51,7 +37,7 @@ public class Exam3 implements Runnable {
 		for (Thread thread : at)
 			thread.join();
 
-		result = (double) DotLessThanOne / (double) TotalDot;
+		result = (double) DotLessThanOne / (double) MAX_DOT;
 		System.out.printf("계산되어 나온 값은 %f입니다.\n", result);
 		System.out.printf("실제 PI/4의 값은 %f입니다.\n", PI / 4);
 		System.out.println("우왕 되게 비슷하당");
